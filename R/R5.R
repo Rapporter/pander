78c7e88c7bc33cc9087acf8968044b1b08f2dcb8
Gergely Daróczi <gergely@snowl.net>
2012-05-26 00:32:25 +0200

    add: exporting feature

--- R/R5.R
+++ R/R5.R
@@ -1,26 +1,27 @@
 #' @export
-report <- setRefClass('report', fields = list('author' = 'character', 'title' = 'character', 'date' = 'character', 'body' = 'list'))
+Pandoc <- setRefClass('Pandoc', fields = list('author' = 'character', 'title' = 'character', 'date' = 'character', 'body' = 'list', 'format' = 'character'))
 
-report$methods(initialize = function(author = 'Anonymous', title = base::sprintf('%s\'s report', author), date = base::date(), ...) {
+Pandoc$methods(initialize = function(author = 'Anonymous', title = base::sprintf('%s\'s report', author), date = base::date(), format = 'pdf', ...) {
 
     .self$author <- author
-    .self$title <- title
+    .self$title  <- title
     .self$date   <- date
+    .self$format <- format
     callSuper(...)
 
 })
 
-report$methods(add = function(x) .self$body <- c(.self$body, list(x)))
-report$methods(add.paragraph = function(x) .self$body <- c(.self$body, list(pandoc.p.return(x))))
+Pandoc$methods(add = function(x) .self$body <- c(.self$body, list(x)))
+Pandoc$methods(add.paragraph = function(x) .self$body <- c(.self$body, list(pandoc.p.return(x))))
 
-report$methods(show = function(x) {
+Pandoc$methods(show = function(x) {
 
     ## starting timer for proc.time
     timer  <- proc.time()
 
     ## show header
     cat(pandoc.header(.self$title, style = 'setext'))
-    cat(' written by *', .self$author, '* at ', .self$date, '\n', sep = '')
+    cat(' written by *', .self$author, '* at *', .self$date, '*\n', sep = '')
     cat('\n', sprintf(' This report holds %s block(s).', length(.self$body)), '\n')
 
     ## show body (if not empty)
@@ -28,7 +29,7 @@ report$methods(show = function(x) {
 
         cat(pandoc.horizontal.rule())
 
-        lapply(.self$body, Pandoc)
+        lapply(.self$body, pander)
 
         ## show proc.tim
         cat(pandoc.horizontal.rule())
@@ -36,5 +37,25 @@ report$methods(show = function(x) {
 
     } else
         cat('\n)')
+
+})
+
+Pandoc$methods(export = function(x) {
+
+    f     <- tempfile('pander-', getwd())
+    fp    <- sprintf('%s.md', f)
+    fe    <- sprintf('%s.%s', f, .self$format)
+    timer <- proc.time()
+
+    ## create pandoc file
+    cat(pandoc.title.return(.self$title, .self$author, .self$date), file = fp)
+    lapply(.self$body, function(x) cat(paste(capture.output(pander(x)), collapse = '\n'), file = fp, append = TRUE))
+
+    ## convert
+    system(sprintf('pandoc %s -o %s', shQuote(fp), shQuote(fe)), intern = TRUE)
+
+    ## return
+    cat('\nExported to *', f, '.[md|', format, ']* under ', as.numeric(proc.time() - timer)[3], ' seconds.\n\n', sep = '')
+    return(invisible(fp))
 
 })
