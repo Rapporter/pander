909d6fad399493ba999e71530364297246e1b6d0
Gergely Daróczi <gergely@snowl.net>
2012-08-25 00:11:56 +0200

    fix: checking for blank captions

--- R/helpers.R
+++ R/helpers.R
@@ -784,10 +784,9 @@ pandoc.table.return <- function(t, caption = NULL, digits = panderOptions('digit
             res <- paste0(res, '\n\n')
 
         ## (optional) caption
-        if (caption == '')
-            caption <- NULL
         if (!is.null(caption))
-            res <- sprintf('%sTable: %s\n\n', res, caption)
+            if (caption != '')
+                res <- sprintf('%sTable: %s\n\n', res, caption)
 
         return(res)
 
