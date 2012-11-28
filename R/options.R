576db125ae4164388fbed973a044c218dfd43470
Gergely Daróczi <gergely@snowl.net>
2012-11-28 13:49:27 +0100

    DRY

--- R/options.R
+++ R/options.R
@@ -132,17 +132,17 @@ masked.plots$plot <- masked.plots$barplot <- masked.plots$lines <- masked.plots$
         if (is.null(mc$col.clus))
             mc$col.clus <- cs
     }
-    if (fn %in% c('pairs', 'stripchart')) {
-        doAddGrid <- FALSE
-        par(fg = fc)
-    } else {
-        if (panderOptions('graph.boxes'))
-            par(fg = gc)
-        else
-            par(fg = bc)
-    }
+    par(fg = gc)
+
     if (fn == 'pie')
         mc$col <- cs
+
+    ## remove boxes
+    if (!panderOptions('graph.boxes'))
+        par(bty = 'n', xaxt = 'n', yaxt = 'n')
+
+    if (fn %in% c('pairs', 'stripchart'))
+        doAddGrid <- FALSE
 
     ## call
     mc[[1]] <- fn.orig
