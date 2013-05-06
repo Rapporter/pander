789713e52f835c5baa7f12477af7fa823a30ab57
Gergely Daróczi <gergely@snowl.net>
2013-05-06 21:27:09 +0200

    fix: not just 2d tables, but e.g. matrices need specified column-based check

--- R/pandoc.R
+++ R/pandoc.R
@@ -657,10 +657,10 @@ pandoc.table.return <- function(t, caption, digits = panderOptions('digits'), de
     res <- ''
 
     ## round numbers & cut digits & apply decimal mark & optionally remove trailing zeros
-    if (!is.null(dim(t)) && dim(t) == 2 && is.table(t)) # sapply does not like 2-dimensional tables
-        t.n <- as.numeric(which(apply(t, 2, is.numeric)))
-    else
+    if (!is.null(dim(t)) && length(dim(t)) == 2 && is.data.frame(t)) # sapply does not like 2-dimensional tables
         t.n <- as.numeric(which(sapply(t, is.numeric)))
+    else
+        t.n <- as.numeric(which(apply(t, 2, is.numeric)))
     if (length(t.n) > 0) {
         t[t.n] <- round(t[t.n], round)
         if (!keep.trailing.zeros) {
