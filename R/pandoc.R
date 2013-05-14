a990670014b2a72e0c6a4d2ba3fb8e360072204c
Gergely Daróczi <gergely@snowl.net>
2013-05-14 22:22:55 +0200

    fix #35: only update colnames based on rownames if colnames exist

--- R/pandoc.R
+++ R/pandoc.R
@@ -774,7 +774,8 @@ pandoc.table.return <- function(t, caption, digits = panderOptions('digits'), de
     if (length(t.rownames) != 0) {
 
         t.rownames <- split.large.cells(t.rownames)
-        t.colnames <- c('&nbsp;', t.colnames)
+        if (!is.null(t.colnames))
+            t.colnames <- c('&nbsp;', t.colnames)
         t.width <- c(max(sapply(strsplit(t.rownames, '\n'), function(x) max(nchar(x, type = 'width'), 0))), t.width)
         t.width[1] <- t.width[1] + 2
 
