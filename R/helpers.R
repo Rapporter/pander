1d61bc6e376aeefa05b46a701cf44f680de5b679
Gergely Daróczi <gergely@snowl.net>
2013-05-24 14:22:50 +0200

    fix: check if rownames are needed

--- R/helpers.R
+++ R/helpers.R
@@ -152,7 +152,7 @@ get.alignment <- function(df) {
 
         if (is.function(a)) {
             row.names <- NULL
-            if (length(dim(df)) < 2 || length(rownames(df)) > 0)
+            if (length(dim(df)) != 1 && length(rownames(df)) > 0)
                 row.names <- a()
             return(c(row.names, a(df)))
         }
