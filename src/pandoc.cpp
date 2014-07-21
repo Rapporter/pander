aae6b3e5ed00d6de52e7f269f8fa3a1132b0bc59
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2014-07-21 15:37:34 -0400

    Working, stable and refactored implementation of tableExpand_cpp

--- src/pandoc.cpp
+++ src/pandoc.cpp
@@ -5,75 +5,64 @@
 using namespace Rcpp;
 
 std::string format_cpp(std::string x, std::string justify, int width){
-  int i, j;
   std::string result = "";
   if (justify == "left"){
     result += x;
-    for (i = x.length(); i < width; i++)
-      result += " ";
+    result += std::string(width, ' ');
   } else if (justify == "right"){
-    for (i = 0; i < width - x.length(); i++)
-      result += " ";
+    result += std::string(width, ' ');
     result += x;
   } else {
-    for (i = 0; i < (width - x.length())/2; i++)
-      result += " ";
+    result += std::string((width - x.length())/2, ' ');
     result += x;
-    for (j = i + x.length(); j < width; j++)
+    for (int j = (width + x.length())/2; j < width; j++)
       result += " ";
   } 
   return result;
 }
-//[[Rcpp::export]]
+
 std::string tableExpand_cpp(CharacterVector cells, IntegerVector colsWidth, CharacterVector justify, CharacterVector sepCols, std::string style) {
-    std::string res = "", temp, word;
-    int i, j;
+    std::string res = "", word, *line;
+    size_t pos;
+    char endline = '\n';
+    std::vector<std::string> cellsC(cells.size()), *resSplit;
+    int i, j, width, maxLengthCells = 0, n = 0;
     bool hasLineBreak = false;
-    int width;
-    // check for having a line break
-    for (i = 0; i < cells.length(); i++){
-      temp = as<std::string>(cells[i]);
-      if (temp.find('\n') != std::string::npos){
+    // check for having a line break and convert to string vector for easiness
+    for (i = 0; i < cellsC.size(); i++){
+      cellsC[i] = std::string(cells[i]);
+      if (cellsC[i].find(endline) != std::string::npos)
         hasLineBreak = true;
-        break;
-      }
     }
     if (hasLineBreak){
       if (style == "simple" || style == "rmarkdown")
         stop("Pandoc does not support newlines in simple or Rmarkdown table format!");
-      int n = cells.length(), maxLengthCells = 0;
-      std::vector<std::string> *resSplit = new std::vector<std::string>[n];
+      n = cellsC.size();
+      resSplit = new std::vector<std::string>[n];
       for (i = 0; i < n; i++){
-        temp = std::string(cells[i]);
-        char *tempStr = new char[cells[i].size()], *end, *pch;
-        std::strcpy(tempStr, temp.c_str());
-        pch = strtok_r(tempStr,"\n", &end);
-        while (pch != NULL)
+        line = &cellsC[i];
+        do
         {
-          word = std::string(pch);
-          resSplit[i].push_back(word);
-          pch = strtok_r(NULL, " ", &end);
+          pos = line->find(endline);
+          if (line->substr(0, pos) != "")
+            resSplit[i].push_back(line->substr(0, pos));
+          line->erase(0, pos + 1);
         }
+        while (pos != std::string::npos);
         if (resSplit[i].size() > maxLengthCells)
           maxLengthCells = resSplit[i].size();
       }
       for (i = 0; i < maxLengthCells; i++){
         CharacterVector newCells(n);
-        for (j = 0; j < n; j++){
-          if (resSplit[j].size() > i)
-            newCells[j] = resSplit[j][i];
-          else
-            newCells[j] = "  ";
-        }
-        res += tableExpand_cpp(newCells, colsWidth, justify, sepCols, style);
-        if (i != (maxLengthCells - 1))
-          res += "\n";
+        for (j = 0; j < n; j++)
+          newCells[j] = resSplit[j].size() > i ? resSplit[j][i] : "  ";
+        res +=  tableExpand_cpp(newCells, colsWidth, justify, sepCols, style);
+        if (i != (maxLengthCells - 1)) // because of collapse usage
+          res += '\n';
       }
     } else {
       res = sepCols[0];
-      for (i = 0; i < cells.length(); i++){
-        std::string temp = as<std::string>(cells[i]);
-        temp = as<std::string>(justify[i]);
+      for (i = 0; i < cells.size(); i++){
         width = colsWidth[i] + std::count(cells[i].begin(), cells[i].end(), '\\');
         std::string fres = format_cpp(as<std::string>(cells[i]), as<std::string>(justify[i]), width);
         res += fres;
