d7976c6457c0bd65821d08e004e8215de9ebb17a
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2014-07-27 16:02:45 -0400

    Added more tests for splitLine and refactored it

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 
-int paste_wrapper(std::string &result, std::string s, char sep, char* pch_word){
+int paste_wrapper(std::string &result, std::string s, char sep, size_t pos){
     char last = *result.rbegin();
     int n = 0;
     if (result.empty() || last == '\n' || last == '\n'){ 
       result += s;
-      if (pch_word != NULL){
+      if (pos != std::string::npos){
             result += "\n";
             n = 0;
       }
@@ -23,33 +23,35 @@ int paste_wrapper(std::string &result, std::string s, char sep, char* pch_word){
 }
 
 void process_hyphenation(std::string &result, std::string &word, int &nline, int &nword, int max_width,  Rcpp::Function hyphen){
-    char *pch_syl, *cword, *end_syllable;
+    char chyphen = '-';
     Rcpp::CharacterVector hyphened_word_vec;
     std::string hyphened_word, syllable;
-    int nsyl;
-    
+    int nsyl, hyph;
+    size_t pos = std::string::npos;
     nline = nline - nword;
     hyphened_word_vec = hyphen(word);
     hyphened_word = std::string(hyphened_word_vec[0]);
-    cword = new char[hyphened_word.length() + 1];
-    strcpy(cword, hyphened_word.c_str());
-    pch_syl = strtok_r(cword, "-", &end_syllable);
-    if (hyphened_word == word){ // no hyphen
-      if (nline == 1){
+
+    pos = hyphened_word.find(chyphen);
+    if (pos == std::string::npos){ // no hyphen
+      if (nline == 0){
         result += word + '\n';
       } else {
         result += '\n' + word;
         nline = nword;
       }
     } else {
-      while (pch_syl != NULL){
-        syllable = std::string(pch_syl);
+      do
+      {
+        pos = hyphened_word.find(chyphen);
+        syllable = hyphened_word.substr(0, pos);
         nsyl = syllable.length();
-        if (nsyl + nline + 1 > max_width){
-          if (nline == 1){ // must add at least something
+        hyph = (pos != std::string::npos) ? 1 : 0;
+        if (nsyl + nline + hyph > max_width){
+          if (nline == 0){ // must add at least something
             result += syllable; 
           } else {
-            if (pch_syl == cword){
+            if (word.substr(0, syllable.length()) == syllable){
               result += '\n' + syllable;
             } else {
               result += "-\n" + syllable;
@@ -57,43 +59,43 @@ void process_hyphenation(std::string &result, std::string &word, int &nline, int
           } 
           nline = nsyl;
         } else {
-          if (pch_syl == cword && nline != 1){
+          if (word.substr(0, syllable.length()) == syllable && nline != 0){
             result += ' ';
           }
           result += syllable;
           nline += nsyl;
         }
-        pch_syl = strtok_r(NULL, "-", &end_syllable);
-      }
+        hyphened_word.erase(0, pos + 1);
+      } while (pos != std::string::npos);
     }
-    delete []cword;
 }
 
 //[[Rcpp::export]]
 std::string splitLine_cpp(std::string str, int max_width, bool use_hyphening, Rcpp::Function hyphen){
-  char *pch_word, *cstr, *end_word;
-  std::string result, word, hyphened_word, syllable;
+  char whitespace = ' ';
+  size_t pos;
+  std::string result, word;
   int nline = 0, nword;
-  cstr = new char[str.length() + 1];
-  strcpy(cstr, str.c_str());
-  // break line into tokens by whitespace and iterate through those tokens
-  pch_word = strtok_r(cstr," ", &end_word);
-  while (pch_word != NULL)
+  
+// break line into tokens by whitespace and iterate through those tokens
+  do
   {
-    word = std::string(pch_word);
-    pch_word = strtok_r(NULL, " ", &end_word);
+    pos = str.find(whitespace);
+    word = str.substr(0, pos);
     nword = word.length();
-    nline = nline + nword + 1;
-    if (nline - 1 > max_width) {
+    nline = nline + nword;
+    if (nline > max_width) {
       if (use_hyphening) {
         process_hyphenation(result, word, nline, nword, max_width, hyphen);
       } else {
-        nline = paste_wrapper(result, word, '\n', pch_word);
+        nline = paste_wrapper(result, word, '\n', pos);
       }
     } else{
-      paste_wrapper(result, word, ' ', NULL);
+      paste_wrapper(result, word, ' ', std::string::npos);
     }
-  }
-  delete [] cstr;
+    nline += 1;
+    str.erase(0, pos + 1);
+  } while (pos != std::string::npos);
+  
   return result;
 }
