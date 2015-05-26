db2518790412366d867682b6bd3892386e80d420
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2015-05-26 11:02:25 +0300

    Added tests for aovlist

--- inst/tests/test-S3.R
+++ inst/tests/test-S3.R
@@ -450,3 +450,18 @@ test_that('pander.anova behaves correctly', {
     pa <- pander_return(a, style='simple')
     expect_true(all(sapply(names(a)[-6], grepl, pa[3])))
 })
+
+test_that('pander.aovlist/pander.summary.aovlist behaves correctly', {
+    options(contrasts=c("contr.helmert", "contr.poly"))
+    N <- c(0,1,0,1,1,1,0,0,0,1,1,0,1,1,0,0,1,0,1,0,1,1,0,0)
+    P <- c(1,1,0,0,0,1,0,1,1,1,0,0,0,1,0,1,1,0,0,1,0,1,1,0)
+    K <- c(1,0,0,1,0,1,1,0,0,1,0,1,0,1,1,0,0,0,1,1,1,0,1,0)
+    yield <- c(49.5,62.8,46.8,57.0,59.8,58.5,55.5,56.0,62.8,55.8,69.5,
+               55.0, 62.0,48.8,45.5,44.2,52.0,51.5,49.8,48.8,57.2,59.0,53.2,56.0)
+    
+    npk <- data.frame(block=gl(6,4), N=factor(N), P=factor(P),
+                      K=factor(K), yield=yield)
+    a <- aov(yield ~  N*P*K + Error(block), npk)
+    pa <- pander_return(a, style='simple')
+    expect_equal(length(pa), 14)
+})
