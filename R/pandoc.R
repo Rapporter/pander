dc9a25e0b4bfed7c171d28c6b99cd642a4e439be
Johannes Ranke <jranke@uni-bremen.de>
2016-11-02 11:10:57 +0100

    Move code handling row/col.names to a later point

--- R/pandoc.R
+++ R/pandoc.R
@@ -632,17 +632,6 @@ pandoc.list <- function(...)
 #' pandoc.table(x, split.cells = 10, use.hyphening = TRUE)
 pandoc.table.return <- function(t, caption, digits = panderOptions('digits'), decimal.mark = panderOptions('decimal.mark'), big.mark = panderOptions('big.mark'), round = panderOptions('round'), missing = panderOptions('missing'), justify, style = c('multiline', 'grid', 'simple', 'rmarkdown'), split.tables = panderOptions('table.split.table'), split.cells = panderOptions('table.split.cells'), keep.trailing.zeros = panderOptions('keep.trailing.zeros'), keep.line.breaks = panderOptions('keep.line.breaks'), plain.ascii = panderOptions('plain.ascii'), use.hyphening = panderOptions('use.hyphening'), row.names, col.names, emphasize.rownames = panderOptions('table.emphasize.rownames'), emphasize.rows, emphasize.cols, emphasize.cells, emphasize.strong.rows, emphasize.strong.cols, emphasize.strong.cells, emphasize.italics.rows, emphasize.italics.cols, emphasize.italics.cells, emphasize.verbatim.rows, emphasize.verbatim.cols, emphasize.verbatim.cells, ...) { #nolint
 
-    if (!missing(row.names)) {
-      if (row.names[1] == FALSE) {
-        rownames(t) <- NULL
-      } else {
-        rownames(t) <- row.names
-      }
-    }
-    if (!missing(col.names)) {
-      colnames(t) <- col.names
-    }
-
     ## expands cells for output
     table.expand <- function(cells, cols.width, justify, sep.cols) {
         .Call('pander_tableExpand_cpp', PACKAGE = 'pander', cells, cols.width, justify, sep.cols, style)
@@ -894,6 +883,16 @@ pandoc.table.return <- function(t, caption, digits = panderOptions('digits'), de
         style <- panderOptions('table.style')
     } else {
         style <- match.arg(style)
+    }
+    if (!missing(row.names)) {
+      if (row.names[1] == FALSE) {
+        rownames(t) <- NULL
+      } else {
+        rownames(t) <- row.names
+      }
+    }
+    if (!missing(col.names)) {
+      colnames(t) <- col.names
     }
     if (is.null(mc$justify)) {
         if (is.null(attr(t, 'alignment'))) {
