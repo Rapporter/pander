a2127d7478dffe3d5468f79c524055a1a8ea8333
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-11-06 01:43:01 +0100

    add title to temp pandoc doc due to pandoc v2.0

--- inst/tests/test-convert.R
+++ inst/tests/test-convert.R
@@ -9,15 +9,18 @@ test_that('openFileInOS works correctly', {
     sink()
 #    expect_equal(openFileInOS(tf), 0)
     expect_error(openFileInOS('NEWS2'))
+    unlink(tf)
 })
 
 test_that('Pandoc.convert works correctly', {
     tf <- tempfile()
-    sink(tf)
+    cat('% test\n', file = tf)
+    sink(tf, append = TRUE)
     pander(mtcars[1:3, 1:4])
     sink()
     res1 <- Pandoc.convert(tf, open = FALSE)
     expect_equal(dirname(tf), dirname(res1))
-    res2 <- Pandoc.convert(text = pandoc.table.return(mtcars[1:3, 1:4]), open = FALSE)
+    res2 <- Pandoc.convert(text = readLines(tf), open = FALSE)
     expect_equal(readLines(res1), readLines(res2)) # text or file gives same result
+    unlink(tf)
 })
